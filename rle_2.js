5ffecd633d67a7e7cb232af9eef3d8fb1edac45d
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2022-12-19 20:27:52 +0500

    Update rle_2.js

--- rle_2.js
+++ rle_2.js
@@ -1,4 +1,3 @@
-
 let fs = require('fs');
 let arg = process.argv;
 let s;
@@ -11,28 +10,36 @@ if (arg[2] == "code"){
             console.error(err);
             return;
         }
+        // ^ Если подаётся агрумент code - читаем файл, задаваемый в 3 аргумент
         s = data.toString();
-        while (i < s.length){
+        // ^ Приводим считанные данные к строке
+        while (i < s.length){ // проходимся по строке
             while(s.charAt(i) == s.charAt(i + k))
                 k++;
             i += k;
+            // ^ пока символы повторяются - прибавляем к счётчику кол-ва повторений текущего символа
             while (k > 255){
                 k -= 255;
                 res += '#' + String.fromCharCode(255) + s.charAt(i - 1);
             }
+            // ^ если счётчик перевалил за 255 - записываем в результат закодированное сообщение формата (#255(ascii)sym)
             if ((k >= 3) || (s.charAt(i - 1) == '#')){
                 res += '#' + String.fromCharCode(k) + s.charAt(i - 1);
             }
+            // ^ если текущих символов >=3 и <256, то записываем в результат закодированное сообщение формата (#k(ascii)sym)
             else{
                 res += s.charAt(i).repeat(k);
             }
+            // ^ иначе в результат записываем символ(ы), стоящий(ие) в изначальной строке
             k = 1;
+            // ^ сбрасываем счётчик
         }
         fs.writeFile('code.txt', res, (err) => {
             if (err){
                 console.err(err);
                 return;
             }
+            // ^ Записываем результат в файл code.txt
         });
     });
 }
@@ -43,24 +50,29 @@ else if (arg[2] == 'decode'){
             console.error(err);
             return;
         }
+        // ^ Если подаётся агрумент decode - читаем файл, задаваемый в 3 аргумент
         s = data.toString();
+        // ^ Приводим считанные данные к строке
         s = s.split('');
-        while (i < s.length) {
+        // ^ разбиваем поэлементно строку в массив
+        while (i < s.length) { // идём по строке
             if (s[i] == '#') {
                 let t = s[i + 1].charCodeAt();
                 res += s[i + 2].repeat(t);
                 i += 3;
-            }
+            } // ^ если встретили # - запсиываем в t количество повторений - в res записываем символ t раз
             else {
                 res += s[i];
                 i++;
             }
+            // ^ если встретили просто символ - записываем в res
         }
         fs.writeFile('decode.txt', res, (err) => {
             if (err){
                 console.error(err);
                 return;
             }
+            // ^ записываем получившийся результат в decode.txt
         });
     });
 }
